1f9b194636bd56d4256cf3c2182442ba7def8006
nsaahi <nsaahi001@gmail.com>
2026-09-17 18:03:51 +0530

    Time: 2 ms (54.13%), Space: 42.3 MB (15.38%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.java
+++ 0052-n-queens-ii/0052-n-queens-ii.java
@@ -3,7 +3,6 @@ class Solution {
     public int totalNQueens(int n) {
         char[][] board = new char[n][n];
 
-        // Fill board with empty cells
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 board[i][j] = '.';
@@ -15,25 +14,18 @@ class Solution {
 
     public int solve(int row, int n, char[][] board) {
 
-        // All queens are placed
         if (row == n) {
             return 1;
         }
 
         int count = 0;
 
-        // Try every column in the current row
         for (int col = 0; col < n; col++) {
 
             if (isSafe(row, col, n, board)) {
 
-                // Choose
                 board[row][col] = 'Q';
-
-                // Explore
                 count += solve(row + 1, n, board);
-
-                // Undo
                 board[row][col] = '.';
             }
         }
@@ -42,15 +34,12 @@ class Solution {
     }
 
     public boolean isSafe(int row, int col, int n, char[][] board) {
-
-        // Check same column
         for (int i = 0; i < row; i++) {
             if (board[i][col] == 'Q') {
                 return false;
             }
         }
 
-        // Check upper-left diagonal
         for (int i = row - 1, j = col - 1;
              i >= 0 && j >= 0;
              i--, j--) {
@@ -60,7 +49,6 @@ class Solution {
             }
         }
 
-        // Check upper-right diagonal
         for (int i = row - 1, j = col + 1;
              i >= 0 && j < n;
              i--, j++) {
